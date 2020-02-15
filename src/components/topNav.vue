87b382f0a53c4daedb28d38dd4f8951c1070071f
Mateusz Boruszczak <borsuk1253@gmail.com>
2020-02-15 17:55:03 +0100

    changed own transition function to build-in functionality

--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -5,15 +5,15 @@
         <li><router-link to="/my-tasks" class="btn btn-uhe">My Tasks</router-link></li>
         <li><router-link to="/general" class="btn btn-uhe">All Tasks</router-link></li>
       </ul>
-      <!-- <button class="btn btn-logout" v-on:click="logout">Logout</button> -->
-      <button class="btn btn-logout" v-on:click="showUserMenu">User</button>
-      <ul class="user-menu" ref="user-menu">
-        <li>
-          <router-link to="/my-profile">Profile</router-link>
-        </li>
-          <li  v-on:click="logout"><span>Logout</span></li>
-      </ul>
-
+      <button class="btn btn-logout" v-on:click="show_user_panel = !show_user_panel">User</button>
+      <transition name="expand">
+        <ul class="user-menu" ref="user-menu" v-show="show_user_panel">
+          <li>
+            <router-link to="/my-profile">Profile</router-link>
+          </li>
+            <li  v-on:click="logout"><span>Logout</span></li>
+        </ul>
+      </transition>
     </nav>
 </template>
 
@@ -21,32 +21,16 @@
 
 import { bus } from '../main'
 import { fireAuth } from '../main'
-import { setTimeout } from 'timers';
 
 export default {
   data() {
     return {
-
+      show_user_panel: 0,
     };
   },
   methods: {
     showTaskForm: function() {
       bus.$emit("showTaskPop", 1)
-    },
-    showUserMenu: function() {
-      // this.$refs["user-menu"].classList.toggle("show")
-      const user_menu = this.$refs["user-menu"]
-      if(!user_menu.classList.contains("show")) {
-      user_menu.style.display = "block"
-      setTimeout(() => {
-        user_menu.classList.add("show")
-      }, 50)
-      } else {
-        user_menu.classList.remove("show")
-      setTimeout(() => {
-        user_menu.style.display = "none"
-      }, 250)
-      }
     },
     logout: function() {
       fireAuth.signOut().then(() => {
